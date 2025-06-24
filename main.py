b85d79443b8d786331d2be7e818577fd02b218aa
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-24 22:00:34 +0530

    As

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ emerg={}
 emergency={}
 medi={}
 medicine={}
+assigned_rooms={}
 
 def register_patient(patient_id, personal_info, medical_history, insurance_info):
     add.update({"patient_id":patient_id,
@@ -72,7 +73,20 @@ def track_medication_inventory(medication_id, quantity, expiry_date, supplier):
         medication_id:{"quantity":quantity,"expiry_date":expiry_date,"supplier":supplier}
     })
     medicine.update(medi)
-# def assign_room(patient_id, room_type, admission_date, expected_duration)
+def assign_room(patient_id, room_type, admission_date, expected_duration):
+    rooms={
+        "general-ward":0,
+        "private room":5,
+        "emergency room":10
+    }
+    room_type=room_type.lower()
+    if rooms[room_type]>0:
+        assigned_room.update({patient_id:{"room_type":room_type,"admission_date":admission_date,"exp_duaration":expected_duration
+        }})
+        rooms[room_type]-=1
+    else:
+        print(f"Currently the {room_type} is unavailable Opt for other rooms")
+assign_room(123,"GEneral-Ward","21-02-2021",12)
 # def calculate_treatment_cost(patient_id, treatment_plan, insurance_details)
 # def generate_patient_report(patient_id, report_type)
 # def analyze_hospital_efficiency(metrics_type, time_period)
