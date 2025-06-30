b074b186dcd7ef02e1a502c7845c6e21661087c5
Adarshkamat <adarshkumarkamat23@ifheindia.org>
2025-06-30 11:42:25 +0530

    updated

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ patient_report={}
 discharged_patients={}
 
 hopspital_efficiency=[]
-records={}
+records=[]
 
 def register_patient(patient_id,name,personal_info, medical_history, insurance_info):
     if patient_id in emergency:
@@ -34,11 +34,10 @@ def register_patient(patient_id,name,personal_info, medical_history, insurance_i
         print("Already Registered")
 register_patient(123,"Romi",{"age":21,"gender":"Male","blood_type":"O+"},"Diabetes",{"company":"Star Health","policy_no":92394412})
 
-# shift_schedule = [
-#     {"day": "Monday", "start_time": "08:00", "end_time": "16:00", "location": "ICU"},
-# ]
+
 
 def add_medical_staff(staff_id, name, specialization, shift_schedule, contact_info):
+ if staff_id not in medical_staffs:
    medical_staffs.update({
        staff_id:{"name":name,
             "specialization":specialization,
@@ -46,6 +45,8 @@ def add_medical_staff(staff_id, name, specialization, shift_schedule, contact_in
             "contact_info":contact_info}   
    })
    print(f"Medical Staff ('ID'{staff_id}) is added ")
+ else:
+     print("The Staff already exists")
 add_medical_staff(101123,"Dr.Subhash Rao","General Doctor",[
     {"day": "Monday", "shift": "Morning", "start": "08:00", "end": "14:00", "location": "OPD"},
     {"day": "Tuesday", "shift": "Evening", "start": "14:00", "end": "20:00", "location": "OPD"},
@@ -74,77 +75,86 @@ add_medical_staff(101143,"Dr.Santosh","Neurologist",[
 ], 7800978543)
 
 
-def schedule_appointment(patient_id, doctor_id, appointment_date, appointment_type,charges):
+
+def schedule_appointment(patient_id, doctor_id, appointment_date, appointment_type):
+    appt_datetime = datetime.strptime(appointment_date, "%d-%m-%Y %H:%M")
+
+    if doctor_id not in appointment:
+        appointment[doctor_id] = []
+    for appt in appointment[doctor_id]:
+        existing_time = datetime.strptime(appt["appointment_date"], "%d-%m-%Y %H:%M")
+        if existing_time == appt_datetime:
+            print("Appointment Conflict")
+            return
+
     if patient_id in patient and doctor_id in medical_staffs:
-        appointment.update({
-        doctor_id:{"patient_id":patient_id ,
-                    "appointment_date":appointment_date,
-                    "appointment_type":appointment_type,
-                    "charges":charges}
-    })
+        appointment[doctor_id].append({
+            "patient_id": patient_id,
+            "appointment_date": appointment_date,
+            "appointment_type": appointment_type
+        })
         print(f"""The appointment has been scheduled at {appointment_date}
-with {medical_staffs[doctor_id]["name"]}""")
+with {medical_staffs[doctor_id]['name']}""")
+        patient[patient_id].update({"doctor":doctor_id})
     else:
-        print("Register the patient ")
+        print("Register the patient or doctor first")
 
-schedule_appointment(123, 101123, '21-7-2025', "Consultation",1500)
+
 def doctors_schedule(doctor_id):
-    print(f"{medical_staffs[doctor_id]["name"]} - ({medical_staffs[doctor_id]["specialization"]})")
-    today=datetime.today()
-    print(datetime.strftime(today,"%B %d, %Y"))
+    if doctor_id not in appointment or doctor_id not in medical_staffs:
+        print("Doctor not found or has no appointments")
+        return
 
-    for i in appointment[doctor_id]:
-        if i=="charges":
-            break
-        print(f"{i}:{appointment[doctor_id][i]}")
+    doctor = medical_staffs[doctor_id]
+    print(f"{doctor['name']} - ({doctor['specialization']})")
+
+    today = datetime.today().date()
+    print(datetime.strftime(datetime.today(), "%B %d, %Y"))
+
+    found_today = False
+
+    for appt in appointment[doctor_id]:
+        appt_datetime = datetime.strptime(appt["appointment_date"], "%d-%m-%Y %H:%M")
+        if appt_datetime.date() == today:
+            if not found_today:
+                print("\nToday's Appointments:")
+                found_today = True
+            print(f"{appt_datetime.strftime('%H:%M')} - Patient ID: {appt['patient_id']} ({appt['appointment_type']})")
+
+    if not found_today:
+        print("\nNo appointments today.")
+
+
+schedule_appointment(123, 101123, '30-6-2025 8:00', "Consultation")
+schedule_appointment(123, 101123, '30-6-2025 9:00', "Consultation")
 doctors_schedule(101123)
+
+
         
 
 
 def create_medical_record(patient_id, doctor_id, diagnosis, treatment, prescription):
  
-    if patient_id in appointment[doctor_id]:
-        medical_record.update({
-            patient_id:{"doctor_id":doctor_id, "diagnosis":diagnosis, "treatment":treatment, "prescription":prescription}
-        })
+    for app in appointment[doctor_id]:
+        if patient_id == app["patient_id"]:
+            medical_record.update({
+                patient_id:{"doctor_id":doctor_id, "diagnosis":diagnosis, "treatment":treatment, "prescription":prescription}
+            })
+            print("Medical record Created")
+            break
     else:
         print("Register the patient ")
 
+create_medical_record(123, 101123, "Type 2 Diabetes Mellitus", "Lifestyle changes, blood sugar monitoring, and oral medications", [
+    {"medicine": "Metformin", "dosage": "500mg", "frequency": "Twice a day"},
+    {"medicine": "Glimepiride", "dosage": "2mg", "frequency": "Once a day"},
+])
 
-def process_billing(patient_id, services_list, insurance_coverage):
-    billing.update({
-        patient_id:{
-        "service_list":services_list,
-        "insurance_coverage":insurance_coverage}
-    })
-    
-    
-    if patient_id in assigned_rooms :
-        global admi_date
-        admi_date=datetime.strptime(assigned_rooms[patient_id]["admission_date"], "%d-%m-%Y")
-    if patient_id in discharged_patients:
-        disc_date=datetime.strptime(discharged_patients[patient_id]["discharge_date"],"%d-%m-%Y")
-    doctor=create_medical_record[patient_id]["doctor_id"]
-    print(f"""=== BILLING SUMMARY ===
-Patient: {patient[patient_id]["name"]}
-Admission Date: {datetime.strftime(admi_date, "%B %d, %Y")}
-Discharge Date: {datetime.strftime(disc_date, "%B %d, %Y")}
-Service Charges:
-Room Charges ({(disc_date-admi_date).days}days): {((disc_date-admi_date).days)*assigned_rooms[patient_id]["room_type"]["price"]}
-Doctor Consultation : {appointment[doctor]["charges"]}""")
-    for key,value in services_list:
-        print(f"{key}:{value}")
-    sum_of_services=sum(cost for services,cost in services_list) 
-    Sum_fo_Serices=sum_of_services+{appointment[doctor]["charges"]}+{((disc_date-admi_date).days)*assigned_rooms[patient_id]["room_type"]["price"]}
-    print(f"Total bill : {Sum_fo_Serices})
-    if billing[patient_id]["insurance_coverage"] :
-        after_insurance=(billing[patient_id]["insurance_coverage"])/100*Sum_fo_Serices+Sum_fo_Serices
-        print(f"Insurance ({billing[patient_id]["insurance_coverage"]}) : {after_insurance}")
-        print(f"patient responsibility : {Sum_fo_Serices-after_insurance}")
-    else:
-        print("You have no insurance coverage")
-        print(f"Patient Responsibility : {Sum_fo_Serices}")
-    
+
+
+
+
+
 
 
 def manage_emergency_admission(patient_id, emergency_type, severity_level):
@@ -156,6 +166,11 @@ def manage_emergency_admission(patient_id, emergency_type, severity_level):
                 "medical_history":"Emergency",
                 "insurance_info":None}
     })
+    today=datetime.today()
+    assigned_rooms.update( {patient_id :{"room_type":"emergency room","admission_date":today,"exp_duaration":None}
+                            })
+    rooms["emergency room"]["beds"]-=1
+    patient[patient_id]["room_type"]="emergency room"
    
 
 def track_medication_inventory(medication_id, quantity, expiry_date, supplier):
@@ -169,7 +184,7 @@ def track_medication_inventory(medication_id, quantity, expiry_date, supplier):
         })
         print("The Medicine has been added to the inventory!")
 rooms={
-        "general ward":{"beds":20,
+        "general room":{"beds":20,
         "price":1500},
         "private room":{"beds":15,
         "price":3000},
@@ -186,17 +201,73 @@ def assign_room(patient_id, room_type, admission_date, expected_duration):
     else:
         print(f"Currently the {room_type} is unavailable Opt for other rooms")
 
+assign_room(123,"general room","1-7-2025",4)
+
+def manage_discharge_process(patient_id,admission_date, discharge_date, follow_up_instructions):
+    if patient_id in assigned_rooms:
+        room_ty=patient[patient_id]["room_type"]
+        rooms[room_ty]["beds"]+=1
+        a=patient[patient_id].pop("room_type")
+        print(f"The patient has been Discharged from {a} Room")
+    discharged_patients.update({patient_id:{"discharge_date":discharge_date,"follow_up_instructions":follow_up_instructions}})
+    hopspital_efficiency.append({"patient_id":patient_id,"admission_date":admission_date,"discharge_date":discharge_date})
+   
+
+manage_discharge_process(123,"1-7-2025","4-7-2025","Eat oil free food")
+
+
+    
+
+
+def process_billing(patient_id, services_list, insurance_coverage):
+    billing.update({
+        patient_id:{
+        "service_list":services_list,
+        "insurance_coverage":insurance_coverage}
+    })
+    
+    u=assigned_rooms[patient_id]["admission_date"]
+    admi_date=datetime.strptime(u, "%d-%m-%Y")
+    v=discharged_patients[patient_id]["discharge_date"]
+    disc_date=datetime.strptime(v,"%d-%m-%Y")
+   
+    
+    print(f"""=== BILLING SUMMARY ===
+Patient: {patient[patient_id]["name"]}
+Admission Date: {admi_date.strftime('%B %d,%Y')}
+Discharge Date: {disc_date.strftime('%B %d,%Y')}
+Service Charges:
+
+""")
+    for key,value in services_list.items():
+        print(f"{key}:{value}")
+    sum_of_services=sum(cost for services,cost in services_list.items()) 
+    Sum_fo_Serices=sum_of_services
+    print(f"Total bill : {Sum_fo_Serices}")
+    if billing[patient_id]["insurance_coverage"] :
+        after_insurance=(billing[patient_id]["insurance_coverage"])/100*Sum_fo_Serices
+        print(f"Insurance ({billing[patient_id]["insurance_coverage"]}) : {after_insurance}")
+        print(f"patient responsibility : {Sum_fo_Serices-after_insurance}")
+    else:
+        print("You have no insurance coverage")
+        print(f"Patient Responsibility : {Sum_fo_Serices}")
+    
+process_billing(123, {
+    "Room Charges(3 days):4500"
+    "Doctor Charges":2000,
+    "Consultation": 1500,
+    "X-Ray": 800,
+    "Blood Test": 500
+}, 70)
+
 def calculate_treatment_cost(patient_id, treatment_plan, insurance_details):
     ins=insurance_details
     cost=0
     for plan,price in treatment_plan.items():
         cost=sum(price)
-    if patient_id in assigned_rooms :
         
-        admi_date=datetime.strptime(assigned_rooms[patient_id]["admission_date"], "%d-%m-%Y")
-    if patient_id in discharged_patients:
-        disc_date=datetime.strptime(discharged_patients[patient_id]["discharge_date"],"%d-%m-%Y")
-    doctor=create_medical_record[patient_id]["doctor_id"]
+    admi_date=datetime.strptime(assigned_rooms[patient_id]["admission_date"], "%d-%m-%Y")
+    disc_date=datetime.strptime(discharged_patients[patient_id]["discharge_date"],"%d-%m-%Y")
     print(f"""=== BILLING SUMMARY ===
 Patient: {patient[patient_id]["name"]}
 Admission Date: {datetime.strftime(admi_date, "%B %d, %Y")}
@@ -209,13 +280,14 @@ Discharge Date: {datetime.strftime(disc_date, "%B %d, %Y")}""")
     else:
         print("You have no insurance coverage")
         print(f"Patients Responsibility : {cost}")
-    
+
+
 
 def generate_patient_report(patient_id, report_type):
  if patient_id in medical_record:
     patient_report.update({patient_id:report_type})
     print(f"""Patient Name : {patient[patient_id]["name"]} ('ID'{patient_id})
-          Age : {patient[patient_id]["personal_info"]["age"]}| Gender :{patient[patient_id]["personal_info"]["gender"]}| Blood Type : {patient[patient_id]["personal_info"]["blood_type"]}
+Age : {patient[patient_id]["personal_info"]["age"]}| Gender :{patient[patient_id]["personal_info"]["gender"]}| Blood Type : {patient[patient_id]["personal_info"]["blood_type"]}
 Insurance : {patient[patient_id]["insurance_info"]["company"]} ('ID'{patient[patient_id]["insurance_info"]["policy_no"]})""")
     if patient_id in discharged_patients:
         status = "Discharged"
@@ -223,19 +295,41 @@ Insurance : {patient[patient_id]["insurance_info"]["company"]} ('ID'{patient[pat
         status = "Inpatient"
     else :
         status = "Outpatient"
+    admi_date=datetime.strptime(assigned_rooms[patient_id]["admission_date"], "%d-%m-%Y")
     print(f"Current Status : {status}")
-    print(f"Room : {[patient[patient_id]["room_type"]}")
-    print(f"Admiited : {datetime.strftime(admi_date, "%B %d, %Y")}")
-    print(f"Attending Doctor : ------------ ")
+    print(f"Room : {assigned_rooms[patient_id]["room_type"]}")
+    print(f"Admitted : {datetime.strftime(admi_date, "%B %d, %Y")}")
+    dis=patient[patient_id]["doctor"]
+    doc_name=medical_staffs[dis]["name"]
+
+    print(f"Attending Doctor : {doc_name} ")
     today=datetime.today()
     print(f"Recent Vitals ({datetime.strftime(today,"%B %d, %Y - %H:%M %p")}:)")
-    for key,value in report_type:
+    for key,value in report_type.items():
         print(f"{key}:{value}")
     print("Active Medications : ")
-    for key,value in medical_record[patient_id]["prescription"]:
-        print(f"-{key}:{value}")
+    for i in medical_record[patient_id]["prescription"]:
+        print(f"{i["medicine"]} ({i["dosage"]}) : {i["frequency"]}")
+        
  else:
      print("Create the Medical Record of the pateint")
+
+generate_patient_report(123,{
+    "blood_test": "Blood Test",
+    "urine_test": "Urine Test",
+    "xray": "X-Ray",
+    "mri": "MRI Scan",
+    "ct_scan": "CT Scan",
+    "ultrasound": "Ultrasound",
+    "ecg": "ECG",
+    "biopsy": "Biopsy Report",
+    "pathology": "Pathology Report",
+    "surgery_summary": "Surgery Summary"
+})
+
+
+
+
 def analyze_hospital_efficiency(metrics_type, time_period):
     Sum=0
     for i in rooms:
@@ -244,16 +338,16 @@ def analyze_hospital_efficiency(metrics_type, time_period):
                 Sum+=rooms[i][j]
     TOTAL_BEDS=Sum
     start_date, end_date = time_period
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    start_date = datetime.strptime(start_date, "%d-%m-%Y")
+    end_date = datetime.strptime(end_date, "%d-%m-%Y")
 
     total_stay_days = 0
     total_patients = 0
     occupied_bed_days = 0
 
     for record in hopspital_efficiency:
-        admission = datetime.strptime(record["admission_date"], "%Y-%m-%d")
-        discharge = datetime.strptime(record["discharge_date"], "%Y-%m-%d")
+        admission = datetime.strptime(record["admission_date"], "%d-%m-%Y")
+        discharge = datetime.strptime(record["discharge_date"], "%d-%m-%Y")
 
         if discharge < start_date or admission > end_date:
             continue
@@ -281,15 +375,4 @@ def analyze_hospital_efficiency(metrics_type, time_period):
     else:
         print("Invalid metrics type. Please choose from: 'average_length_of_stay', 'bed_occupancy_rate', or 'patient_throughput'.")
 
-def manage_discharge_process(patient_id,admission_date, discharge_date, follow_up_instructions):
-    if patient_id in assigned_rooms:
-        room=assigned_rooms[patient_id]["room_type"]["beds"]
-        rooms[room]+=1
-        assigned_rooms.pop(patient_id)
-        a=patient.pop(patient[patient_id]["room_type"])
-        print(f"The patient has been Discharged from {a} Room")
-    discharged_patients.update({patient_id:{"discharge_date":discharge_date,"follow_up_instructions":follow_up_instructions}})
-    records.update({"patient_id":patient_id,"admission_date":admission_date,"discharge_date":discharge_date})
-    hopspital_efficiency.append(records)
-    
-
+analyze_hospital_efficiency("bed_occupancy_rate",('1-7-2025','7-7-2025'))
